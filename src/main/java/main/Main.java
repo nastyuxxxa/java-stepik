efc0a232221648adadf1c1e4979386ff7a285442
nastyuxxxa <nastya-kubar@ya.ru>
2024-02-26 23:16:02 +0500

    update code

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,13 +5,6 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import servlets.AllRequestsServlet;
 
-/**
- * @author v.chibrikov
- *         <p>
- *         Пример кода для курса на https://stepic.org/
- *         <p>
- *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
- */
 public class Main {
     public static void main(String[] args) throws Exception {
         AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
